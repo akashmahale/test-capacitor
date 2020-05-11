4c2d5c440b5d80afdc635f3c58995bc8c45184bd
Akash Mahale <akashmahale123@gmail.com>
2020-05-11 15:27:10 +0530

    Device Info

--- android/src/main/java/com/androidshibui/capacitor/testCapacitor.kt
+++ android/src/main/java/com/androidshibui/capacitor/testCapacitor.kt
@@ -4,21 +4,40 @@ import com.getcapacitor.*
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.BatteryManager;
+import java.util.Locale;
+import android.os.StatFs;
+import android.provider.Settings;
+import android.os.Environment;
+import android.content.pm.PackageInfo
 
 @NativePlugin
 class testCapacitor : Plugin() {
 
     @PluginMethod
-    fun getBatteryInfo(call: PluginCall) {
+    fun getInfo(call: PluginCall) {
         var r = JSObject()
         r.put("batteryLevel", getBatteryLevel())
         r.put("isCharging", isCharging())
+        r.put("locale", Locale.getDefault().getLanguage());
+        r.put("memUsed", getMemUsed());
+        r.put("diskFree", getDiskFree());
+        r.put("diskTotal", getDiskTotal());
+        r.put("model", android.os.Build.MODEL);
+        r.put("operatingSystem", "android");
+        r.put("osVersion", android.os.Build.VERSION.RELEASE);
+        r.put("appVersion", getAppVersion());
+        r.put("appBuild", getAppBuild());
+        r.put("platform", getPlatform());
+        r.put("manufacturer", android.os.Build.MANUFACTURER);
+        r.put("uuid", getUuid());
+        r.put("isVirtual", isVirtual());
+
         call.success(r)
     }
 
     private fun getBatteryLevel(): Float {
         var ifilter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
-        var batteryStatus: Intent = getContext().registerReceiver(null, ifilter)
+        var batteryStatus: Intent? = getContext().registerReceiver(null, ifilter)
         var level = -1
         var scale = -1
         if (batteryStatus != null) {
@@ -30,12 +49,57 @@ class testCapacitor : Plugin() {
 
     private fun isCharging(): Boolean {
         var ifilter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
-        var batteryStatus: Intent = getContext().registerReceiver(null, ifilter)
+        var batteryStatus: Intent? = getContext().registerReceiver(null, ifilter)
         if (batteryStatus != null) {
             val status: Int = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1)
             return status == BatteryManager.BATTERY_STATUS_CHARGING || status == BatteryManager.BATTERY_STATUS_FULL
         }
         return false
+    }
+
+    private fun getMemUsed(): Long {
+        val runtime: java.lang.Runtime = java.lang.Runtime.getRuntime()
+        return runtime.totalMemory() - runtime.freeMemory()
+    }
+
+    private fun getDiskFree(): Long {
+        val statFs = StatFs(Environment.getRootDirectory().getAbsolutePath())
+        return statFs.getAvailableBlocksLong() * statFs.getBlockSizeLong()
+    }
+
+    private fun getDiskTotal(): Long {
+        val statFs = StatFs(Environment.getRootDirectory().getAbsolutePath())
+        return statFs.getBlockCountLong() * statFs.getBlockSizeLong()
+    }
+
+    private fun getAppVersion(): String? {
+        return try {
+            val pinfo: PackageInfo = getContext().getPackageManager().getPackageInfo(getContext().getPackageName(), 0)
+            pinfo.versionName
+        } catch (ex: java.lang.Exception) {
+            ""
+        }
+    }
+
+    private fun getAppBuild(): String? {
+        return try {
+            val pinfo: PackageInfo = getContext().getPackageManager().getPackageInfo(getContext().getPackageName(), 0)
+            java.lang.Integer.toString(pinfo.versionCode)
+        } catch (ex: java.lang.Exception) {
+            ""
+        }
+    }
+
+    private fun getPlatform(): String? {
+        return "android"
+    }
+
+    private fun getUuid(): String? {
+        return Settings.Secure.getString(this.bridge.getContext().getContentResolver(), android.provider.Settings.Secure.ANDROID_ID)
+    }
+
+    private fun isVirtual(): Boolean {
+        return android.os.Build.FINGERPRINT.contains("generic") || android.os.Build.PRODUCT.contains("sdk")
     }
 
 }
--- src/web.ts
+++ src/web.ts
@@ -1,4 +1,5 @@
 import { WebPlugin } from '@capacitor/core';
+
 import { testCapacitorPlugin } from './definitions';
 
 export class testCapacitorWeb extends WebPlugin implements testCapacitorPlugin {
@@ -9,7 +10,7 @@ export class testCapacitorWeb extends WebPlugin implements testCapacitorPlugin {
     });
   }
 
-  async getBatteryLevel(): Promise<DeviceBatteryInfo> {
+  async getInfo(): Promise<DeviceInfo> {
     
     return null;
   };
@@ -24,9 +25,68 @@ export { testCapacitor };
 import { registerWebPlugin } from '@capacitor/core';
 registerWebPlugin(testCapacitor);
 
-interface DeviceBatteryInfo {
-
+interface DeviceInfo {
+  /**
+   * A percentage (0 to 1) indicating how much the battery is charged
+   */
   batteryLevel?: number;
-
+  /**
+   * Whether the device is charging
+   */
   isCharging?: boolean;
+  /**
+   * The device locale.
+   */
+  locale?: string;
+  /**
+   * The device model. For example, "iPhone"
+   */
+  model: string;
+  /**
+   * The device platform (lowercase).
+   */
+  platform: 'ios' | 'android' | 'electron' | 'web';
+  /**
+   * The UUID of the device as available to the app. This identifier may change
+   * on modern mobile platforms that only allow per-app install UUIDs.
+   */
+  uuid: string;
+  /**
+   * The current bundle verison of the app
+   */
+  appVersion: string;
+  /**
+   * The current bundle build of the app
+   */
+  appBuild: string;
+  /**
+   * The operating system of the device
+   */
+  operatingSystem: 'ios' | 'android' | 'windows' | 'mac' | 'unknown';
+  /**
+   * The version of the device OS
+   */
+  osVersion: string;
+  /**
+   * The manufacturer of the device
+   */
+  manufacturer: string;
+  /**
+   * Whether the app is running in a simulator/emulator
+   */
+  isVirtual: boolean;
+  /**
+   * Approximate memory used by the current app, in bytes. Divide by
+   * 1048576 to get the number of MBs used.
+   */
+  memUsed?: number;
+  /**
+   * How much free disk space is available on the the normal data storage
+   * path for the os, in bytes
+   */
+  diskFree?: number;
+  /**
+   * The total size of the normal data storage path for the OS, in bytes
+   */
+  diskTotal?: number;
 }
